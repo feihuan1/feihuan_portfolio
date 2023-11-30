3ebb7390a1d6e635001898399d5bda8f8d1cf068
eric <you@example.comericrealtor1997@gmail.com>
2023-11-30 07:48:04 -0800

    typo fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Suspense, useState, useRef, useEffect } from "react";
 
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
-import Island from "../models/island";
+import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
